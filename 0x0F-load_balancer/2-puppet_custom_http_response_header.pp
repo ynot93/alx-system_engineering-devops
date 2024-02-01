38e7b6b8ad09abb252d67849d8ae14be5b3f05e1
Tony Mputhia <tonymmputhia@gmail.com>
2024-02-01 01:27:57 -0800

    Modify custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,41 +1,25 @@
-# Create custom HTTP header response with Puppet
+# Installs a Nginx server with custome HTTP header
 
-# Install Nginx package
-package { 'nginx':
-  ensure  => 'installed',
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
-# Create an index page
-file { 'var/www/html/index.html':
-  ensure  => 'present',
-  content => 'Hello World!',
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
 }
 
-# Define custom header configuration
-$custom_header_config = "server_name _;\n\tadd_header X-Served-By $hostname;"
-
-# Create custom 404 page
-file { '/var/www/html/custom404.html':
-  content => "Ceci n'est pas une page\n",
-  ensure  => present,
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
 }
 
-# Configure Nginx with custom HTTP header and 404 page
-file { 'etc/nginx/sites-enabled/default':
-  ensure  => file,
-  content => "
-    $custom_header_config
-    error_page 404 /custom404.html;
-    location = /custom404.html {
-      root var/www/html;
-      internal;
-    }
-  ",
-  notify  => Service['nginx'],
-}
-
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
