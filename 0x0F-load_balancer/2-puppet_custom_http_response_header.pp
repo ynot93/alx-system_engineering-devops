8130fa90da0f367edc6a8ad147eeffb38aa2675e
Tony Mputhia <tonymmputhia@gmail.com>
2024-02-01 01:20:36 -0800

    Modify custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,23 +2,34 @@
 
 # Install Nginx package
 package { 'nginx':
-  ensure => 'installed',
+  ensure  => 'installed',
 }
 
 # Define custom header configuration
 $custom_header_config = "server_name _;\n\t add_header X-Served-By $hostname;"
 
-# Configure Nginx with custom HTTP header
+# Create custom 404 page
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page\n",
+  ensure  => present,
+}
+
+# Configure Nginx with custom HTTP header and 404 page
 file { 'etc/nginx/sites-enabled/default':
   ensure  => file,
   content => "
     $custom_header_config
+    error_page 404 /custom404.html;
+    location = /custom404.html {
+      root var/www/html;
+      internal;
+    }
   ",
   notify  => Service['nginx'],
 }
 
 # Ensure Nginx service is running and enabled
 service { 'nginx':
-  ensure => running,
-  enable => true,
+  ensure  => running,
+  enable  => true,
 }
